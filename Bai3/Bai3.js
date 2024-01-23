36bac7a95833a02b6ec910dfd31951125d43c976
DungLe <tuandungsonhoapy@gmail.com>
2024-01-23 22:29:21 +0700

    JsBasis

---
+++ Bai3/Bai3.js
@@ -0,0 +1,15 @@
+let a = 6;
+let b = '6';
+// console.log(a === b, 'a: ', typeof a, 'b: ', typeof b);
+// console.log(`abcxuz ${a + b}
+// and result: ${2 * (a-b)}`)
+
+let arr = ['Mu', 'Mancity', 'Liver', 'Chelsea'];
+
+let i = 0;
+while(i < arr.length){
+    console.log(arr[i]);
+    i++;
+}
+
+console.log(i);
