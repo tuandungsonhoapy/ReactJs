972e8c46dccf6b78113ec21855bd3f1695f9032f
DungLe <tuandungsonhoapy@gmail.com>
2024-01-31 00:55:53 +0700

    Create react app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './Styles/global.scss';
+import App from './Views/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
